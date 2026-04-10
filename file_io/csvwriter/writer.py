955168728e523ba37964e2f867fdbc34d4beb122
ZeroDeaths <80154701+Legend108@users.noreply.github.com>
2026-04-10 15:53:31 +0530

    Add files via upload

--- file_io/csvwriter/writer.py
+++ file_io/csvwriter/writer.py
@@ -1,1 +1,14 @@
+import csv
 
+gpu_name = input("GPU name: ")
+
+while True:
+    try:
+        gpu_price = int(input("GPU price: "))
+        break
+    except ValueError:
+        print("Provide a valid gpu price")
+
+with open("gpu.csv", "a") as file:
+    writer = csv.writer(file)
+    writer.writerow([gpu_name, gpu_price])
