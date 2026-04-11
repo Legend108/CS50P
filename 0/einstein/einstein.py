1b200cd287e5530647edf87eb7ffd9b590e63e00
ZeroDeaths <80154701+Legend108@users.noreply.github.com>
2026-04-11 16:15:51 +0530

    Delete 0 directory

--- 0/einstein/einstein.py
+++
@@ -1,7 +0,0 @@
-while True:
-    try:
-        mass = int(input("Enter mass: "))
-        print(mass*300000000*300000000)
-        break
-    except ValueError:
-        print("Enter a valid number")
